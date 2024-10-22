07cb7ca4e610d2042000d48a70d561148e093e56
Corina Samson <126088447+Corina2003@users.noreply.github.com>
2024-10-22 17:44:19 +0300

    Delete Ex11.py

--- Ex11.py
+++
@@ -1,8 +0,0 @@
-#Write a function that will order a list of string tuples based on the 3rd character of the 2nd element in the tuple. Example:
-
-#('abc', 'bcd'), ('abc', 'zza')] ==> [('abc', 'zza'), ('abc', 'bcd')]
-def sort_by_3rd_char(tuples_list):
-    return sorted(tuples_list, key=lambda x: x[1][2])
-
-result= sort_by_3rd_char([('abc', 'bcd'), ('abc', 'zza')])
-print(result)
